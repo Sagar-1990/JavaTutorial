bf80f62f8402bcfc43c624fcc8af9c5b3bbcf400
Sagar-1990 <abhisheksagar49@gmail.com>
2024-07-07 22:28:35 +0530

    FunctionsExamle

---
+++ Basic-java-t/src/FunctionsExample.java
@@ -0,0 +1,29 @@
+public class FunctionsExample {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        FunctionsExample fe=new FunctionsExample();
+        fe.SayHello();
+
+        Welcome();      // No need to define function variable if function in same class
+
+        int anil =20;
+        int sam =30;
+         getDaysFromAge(anil);
+         getDaysFromAge(sam);
+
+    }
+
+    public void SayHello(){
+        System.out.println("Hello from Sagar");
+
+    }
+
+    public static void Welcome(){
+        System.out.println("welcome Abhishek");
+    }
+
+    public static void getDaysFromAge(int age) {
+        System.out.println("User age in days is ");
+        System.out.println(age * 365);
+    }
+}
