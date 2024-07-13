95c3ad38d5c5727a670e0dc15d9a09c0d3b62db4
Sagar-1990 <abhisheksagar49@gmail.com>
2024-07-13 20:43:41 +0530

    ForloopExample

---
+++ Basic-java-t/src/LoopExample.java
@@ -0,0 +1,5 @@
+public class LoopExample {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
--- Basic-java-t/src/NestedLoopExample.java
+++ Basic-java-t/src/NestedLoopExample.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class NestedLoopExample {
+    public static void main(String[] args){
+//        for(int i=1; i<=10;i++){
+//            System.out.println("I value is "+ i);
+//            for(int j=1; j<=5 ; j++){
+//                System.out.println(j);
+//            }
+//        }
+
+//      Example for loop inside in loop
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter max value i");
+        int userVal = scan.nextInt();
+        System.out.println("Enter max value2 j");
+        int value2 = scan.nextInt();
+
+
+
+        for(int i=1;i<=userVal;i++){
+            System.out.println("loop count = "+i);
+            for (int j = 1; j <= value2; j++){
+                System.out.println(j);
+            }
+        }
+
+    }
 }
