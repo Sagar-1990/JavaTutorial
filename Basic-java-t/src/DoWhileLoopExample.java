01b0de09c4f4688296551025dda22912adfa8758
Sagar-1990 <abhisheksagar49@gmail.com>
2024-07-16 08:21:23 +0530

    DowhileLoopExercise

---
+++ Basic-java-t/src/DoWhileLoopExample.java
@@ -0,0 +1,17 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args){
+
+//        int item=10;
+//        do{
+//            System.out.println(item);
+//            item--;
+//        }while (item>5);
+
+
+        int item=50;
+        do{
+            System.out.println(item);
+            item-=5;
+        }while (item>5);
+    }
+}
--- Basic-java-t/src/WhileLoopExample.java
+++ Basic-java-t/src/WhileLoopExample.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
+
 public class WhileLoopExample {
+    public static void main(String[] args){
+
+        System.out.println("While loop will be here");
+//
+//        int item = 0;
+//        while (item<10){
+//            item++;
+//        }
+//        System.out.println(item);
+        System.out.println("Please enter mas value of loop");
+        Scanner scan = new Scanner(System.in);
+        int maxData = scan.nextInt();
+        int data =0;
+        while (data<maxData){
+            data++;
+            System.out.println(data);
+        }
+    }
 }
