b7831307d8dbbc2796046cfcfa68d7f760fce968
Sagar-1990 <abhisheksagar49@gmail.com>
2024-09-12 23:37:04 +0530

    throw and throws

--- Basic-java-t/src/ThrowThrowsExample.java
+++ Basic-java-t/src/ThrowThrowsExample.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ThrowThrowsExample {
+public class ThrowThrowsExample {
+    public static void main(String[] args){
+//        try{
+//            int a = 10;
+//            System.out.println(10 / 0.5);
+//            if (a==10) {
+//                System.out.println("Hello");
+//                throw new ArithmeticException("test");
+//            }
+//        }  catch (ArithmeticException e){
+//            System.out.println("some error" + e);
+//        }
+
+        try {
+            checkage();
+        } catch (ArithmeticException e){
+            System.out.println("Some error" + e);
+        }
+    }
+    public static void checkage() throws ArithmeticException{
+        System.out.println(18/0);
+    }
 }
--- Basic-java-t/src/TryCatchExample.java
+++ Basic-java-t/src/TryCatchExample.java
@@ -1,2 +1,26 @@
+//public class TryCatchExample {
+//    public static void main(String[] args) {
+//        try {
+//            int result = 10 / 0;  // This will throw an ArithmeticException
+//            System.out.println("Result: " + result);
+//        } catch (ArithmeticException e) {
+//            System.out.println("Exception caught: Division by zero is not allowed.");
+//        }
+//        System.out.println("Program continues...");
+//    }
+//}
+
+
 public class TryCatchExample {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception caught");
+        } finally {
+            System.out.println("This block is always executed.");
+        }
+        System.out.println("Hello Next code");
+    }
 }
